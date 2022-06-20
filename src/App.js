841c36f895d2b1c97720d459e92b270a5e16c78d
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-20 23:06:14 +0600

    matches follower getting success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import Influencer1 from "zgodofficial8003followers.json";
 
+import { useEffect, useState } from "react";
+
+// import Influencer2 from "hastarbtc8008followers.json";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [Influencer1, setInfluencer1] = useState([]);
+    const [Influencer2, setInfluencer2] = useState([]);
+    useEffect(() => {
+        fetch("zgodofficial8003followers.json")
+            .then((res) => res.json())
+            .then((data) => setInfluencer1(data));
+    }, []);
+    useEffect(() => {
+        fetch("hastarbtc8008followers.json")
+            .then((res) => res.json())
+            .then((data) => setInfluencer2(data));
+    }, []);
+
+    const data1 = [];
+    const data2 = [];
+    Influencer1.forEach((activity) => {
+        data1.push(activity);
+    });
+
+    Influencer2.forEach((activity) => {
+        data2.push(activity);
+    });
+    const infu1 = [];
+    const infu2 = [];
+    data1.map((user) => {
+        return infu1.push(user.username);
+    });
+    // console.log(infu1);
+    data2.map((user) => {
+        return infu2.push(user.username);
+    });
+    // console.log(infu2);
+
+    const intersection = infu1.filter((element) => infu2.includes(element));
+
+    return (
+        <div>
+            <h1>User List</h1>
+            {intersection.map((user, index) => (
+                <div key={index}>
+                    <h1>{user}</h1>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
